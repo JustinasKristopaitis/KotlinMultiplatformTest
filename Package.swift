4c5b03cf9f63547d0fe7e2d34608e21d3eb59205
Justinas Kristopaitis <53445141+JustinasKristopaitis@users.noreply.github.com>
2023-01-24 13:34:02 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 
-let remoteKotlinUrl = "https://maven.pkg.github.com/touchlab/KMMBridgeKickStart/co/touchlab/kmmbridgekickstart/allshared-kmmbridge/0.13.0/allshared-kmmbridge-0.13.0.zip"
+let remoteKotlinUrl = "https://maven.pkg.github.com/JustinasKristopaitis/KMMBridgeKickStart/co/JustinasKristopaitis/KotlinMultiplatformTest/allshared-kmmbridge/0.13.0/allshared-kmmbridge-0.13.0.zip"
 let remoteKotlinChecksum = "fe57822fa8ae5806e791558b1a632c3cd3af6f185d8e866ff4e132f40f68a6d4"
 let packageName = "allshared"
 
