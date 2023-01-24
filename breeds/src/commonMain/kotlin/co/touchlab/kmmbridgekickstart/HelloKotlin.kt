a6ac0c3f88b89df3e72ebd0170b7995074b86392
Justinas Kristopaitis <justinas.kristopaitis@dogo.app>
2023-01-24 18:38:12 +0200

    update text

--- breeds/src/commonMain/kotlin/co/touchlab/kmmbridgekickstart/HelloKotlin.kt
+++ breeds/src/commonMain/kotlin/co/touchlab/kmmbridgekickstart/HelloKotlin.kt
@@ -2,6 +2,6 @@ package co.touchlab.kmmbridgekickstart
 
 class HelloKotlin {
     fun sayHello(): String {
-        return "Hello from Touchlab!"
+        return "Hello from kotlin. I would like to be implemented in Dogo, and used for Dogo tracking things, as it seems you have a mess over there!"
     }
 }
