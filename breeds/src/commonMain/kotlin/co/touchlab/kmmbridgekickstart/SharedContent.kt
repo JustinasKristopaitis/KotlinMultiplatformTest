cd98bfcb37e5fe59cc8de326f132fc8b8decc740
Justinas Kristopaitis <justinas.kristopaitis@dogo.app>
2023-01-27 13:41:22 +0200

    update filename

--- breeds/src/commonMain/kotlin/co/touchlab/kmmbridgekickstart/SharedContent.kt
+++ breeds/src/commonMain/kotlin/co/touchlab/kmmbridgekickstart/SharedContent.kt
@@ -2,6 +2,6 @@ package co.touchlab.kmmbridgekickstart
 
 class SharedCode {
     fun saySomething(textToWrite: String): String {
-        return "I am kotlin. Thanks for passing me:" + textToWrite
+        return "I am kotlin. Thanks for passing me:$textToWrite"
     }
 }
