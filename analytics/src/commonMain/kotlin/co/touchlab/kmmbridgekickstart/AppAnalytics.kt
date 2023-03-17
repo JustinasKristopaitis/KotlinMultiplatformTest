cf5bdeb1d22e16237a8d62b806b1a37adf7b6f18
Justinas Kristopaitis <justinas.kristopaitis@dogo.app>
2023-03-17 13:43:27 +0200

    add tracking

--- analytics/src/commonMain/kotlin/co/touchlab/kmmbridgekickstart/AppAnalytics.kt
+++ analytics/src/commonMain/kotlin/co/touchlab/kmmbridgekickstart/AppAnalytics.kt
@@ -5,4 +5,41 @@ class AppAnalytics internal constructor() {
     fun appStarted() {
         sendEvent("appStarted")
     }
+
+    fun trackTrickRating(event: TrackingEventName, trickId: String, rating: Int): AppEvent {
+        return AppEvent(
+            name = event,
+        parameters = mapOf(
+                    "trick_id" to trickId,
+            "rating" to rating
+        )
+        )
+    }
+}
+
+data class AppEvent(
+    val name: TrackingEventName,
+    val parameters: Map<String, Any>?
+)
+
+data class TrackingEventName(val rawValue: String) {
+    override fun equals(other: Any?): Boolean {
+        return other is TrackingEventName && other.rawValue == rawValue
+    }
+}
+
+object TrackingEventNames {
+    val trickRated = TrackingEventName("trick_rated")
+}
+
+object AppEvents {
+    fun trickRated(trickId: String, rating: Int): AppEvent {
+        return AppEvent(
+            name = TrackingEventNames.trickRated,
+            parameters = mapOf(
+                "trick_id" to trickId,
+                "rating" to rating
+            )
+        )
+    }
 }
