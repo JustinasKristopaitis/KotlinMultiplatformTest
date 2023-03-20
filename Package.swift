c23667e82f4f2cef86cef7888273341f412d873d
runner <runner@Mac-1679314390112.local>
2023-03-20 12:17:49 +0000

    KMM SPM package release for 0.2.12

--- Package.swift
+++ Package.swift
@@ -1,10 +1,9 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-
-let remoteKotlinUrl = "https://maven.pkg.github.com/touchlab/KMMBridgeKickStart/co/touchlab/kmmbridgekickstart/allshared-kmmbridge/0.13.0/allshared-kmmbridge-0.13.0.zip"
-let remoteKotlinChecksum = "fe57822fa8ae5806e791558b1a632c3cd3af6f185d8e866ff4e132f40f68a6d4"
-let packageName = "allshared"
+let remoteKotlinUrl = "https://maven.pkg.github.com/JustinasKristopaitis/KotlinMultiplatformTest/github/justinasKristopaitis/kotlinmultiplatformtest/allshared-kmmbridge/0.2.12/allshared-kmmbridge-0.2.12.zip"
+let remoteKotlinChecksum = "f7a6f1892538597b17489fec6b710a8246d46c2a1db189b21ee169f1c92842fb"
+let packageName = "DogoTracker"
 
 let package = Package(
     name: packageName,
